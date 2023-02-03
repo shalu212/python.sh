1e78f1d44de4f5a01cb7d26f9ac74fb075cb49cf
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-03 22:12:06 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,26 +2,21 @@
 sudo apt update
 sudo apt-get full upgrade -y
 sudo apt-get install python3-pip -y
+apt-get install sox ffmpeg libcairo2 libcairo2-dev
+apt-get install texlive-full
+pip3 install manimlib 
+pip3 install manimce
 git clone https://github.com/shalu212/Agri.git
 cd Agri
-RUN pip install --upgrade pip
-RUN pip uninstall pycairo
-RUN pip install pycairo
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
 git clone https://github.com/shalu212/Fuel-Consumption-Rating.git
 cd Fuel-Consumption-Rating
-RUN pip install --upgrade pip
-RUN pip uninstall pycairo
-RUN pip install pycairo
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
 git clone https://github.com/shalu212/fish.git
 cd fish
-RUN pip install --upgrade pip
-RUN pip uninstall pycairo
-RUN pip install pycairo
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
