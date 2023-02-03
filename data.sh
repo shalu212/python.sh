3438e032fb0b2ae130cf7653dda84069981b479c
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-03 21:58:30 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,18 +4,24 @@ sudo apt-get full upgrade -y
 sudo apt-get install python3-pip -y
 git clone https://github.com/shalu212/Agri.git
 cd Agri
-pip install --upgrade pip
+RUN pip install --upgrade pip
+RUN pip uninstall pycairo
+RUN pip install pycairo
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
 git clone https://github.com/shalu212/Fuel-Consumption-Rating.git
 cd Fuel-Consumption-Rating
-pip install --upgrade pip
+RUN pip install --upgrade pip
+RUN pip uninstall pycairo
+RUN pip install pycairo
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
 git clone https://github.com/shalu212/fish.git
 cd fish
-pip install --upgrade pip
+RUN pip install --upgrade pip
+RUN pip uninstall pycairo
+RUN pip install pycairo
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
