05b624a7623b0ba5f3d751c1c1b0680bff9b5b75
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-03 21:44:51 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,15 +4,15 @@ sudo apt-get full upgrade -y
 sudo apt-get install python3-pip -y
 git clone https://github.com/shalu212/Agri.git
 cd Agri
-pip3 freeze > requirements.txt
+pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
 git clone https://github.com/shalu212/Fuel-Consumption-Rating.git
 cd Fuel-Consumption-Rating
-pip3 freeze > requirements.txt
+pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
 git clone https://github.com/shalu212/fish.git
 cd fish
-pip3 freeze > requirements.txt
+pip3 install -r requirements.txt
 screen -m -d python3 app.py
