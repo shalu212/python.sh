86d975c24a0aa3347d8f4749001fa65d8c417f9c
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-05 17:06:12 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -10,8 +10,8 @@ nohup python3 app.py &
 git clone https://github.com/shalu212/flight-perdiction.git
 cd /flight-perdiction
 pip3 install -r requirements.txt
-nohup python3 app.py &
+python3 app.py 
 git clone https://github.com/shalu212/fish.git
 cd fish
 pip3 install -r requirements.txt
-nohup python3 app.py 
+nohup python3 app.py &
