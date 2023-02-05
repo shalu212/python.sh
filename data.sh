f4765bf2ad7ef1eba219af8091ccf23ed5a24696
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-05 12:38:51 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -7,14 +7,14 @@ sudo apt-get install pkg-config -y
 git clone https://github.com/shalu212/Agri.git
 cd Agri
 pip3 install -r requirements.txt
-python3 app.py &
+nohup python3 app.py &
 
 https://github.com/shalu212/Medical-Insurance.git
 cd Medical-Insurance
 pip3 install -r requirements.txt
-python3 app.py &
+nohup python3 app.py &
 
 git clone https://github.com/shalu212/fish.git
 cd fish
 pip3 install -r requirements.txt
-python3 app.py &
+nohup python3 app.py &
