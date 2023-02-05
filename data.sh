cd1165b5e4ae92382df67af648a5e9c1969dcbd7
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-05 12:42:49 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,12 +2,11 @@
 sudo apt update
 sudo apt-get full upgrade -y
 sudo apt-get install python3-pip -y
-sudo apt-get install pkg-config -y
 
 git clone https://github.com/shalu212/Agri.git
 cd Agri
 pip3 install -r requirements.txt
-nohup python3 app.py &
+python3 app.py &
 
 https://github.com/shalu212/Medical-Insurance.git
 cd Medical-Insurance
