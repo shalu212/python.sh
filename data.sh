ccd4123f157f4cfad03c1936d3e1a29de44415c1
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-05 16:24:29 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -6,14 +6,14 @@ sudo apt-get install python3-pip -y
 git clone https://github.com/shalu212/Agri.git
 cd Agri
 pip3 install -r requirements.txt
-screen -m -d python3 app.py
+python3 app.py
 
 git clone https://github.com/shalu212/flight-perdiction.git
 cd /flight-perdiction
 pip3 install -r requirements.txt
-screen -m -d python3 app.py
+python3 app.py
 
 git clone https://github.com/shalu212/fish.git
 cd fish
 pip3 install -r requirements.txt
-screen -m -d python3 app.py
+python3 app.py
